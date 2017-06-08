52fee4dbe10ab57336a72b06cf10ba5cfaba5618
Bruno <onurbalmeyda@gmail.com>
2017-06-08 10:55:35 +0100

    Create aws_counter.sh

--- aws/aws_counter.sh
+++ aws/aws_counter.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-num_vpcs=$(aws ec2 describe-vpcs | grep -c VPCS)
+num_vpcs = $(aws ec2 describe-vpcs | grep -c VPCS)
 echo "# VPC: $num_vpcs"
 
-num_ec2=$(aws ec2 describe-instance-status | grep -c "INSTANCESTATUSES")
+num_ec2 = $(aws ec2 describe-instance-status | grep -c "INSTANCESTATUSES")
 echo "# EC2: $num_ec2"
 
-num_rds=$(aws rds describe-db-instances | grep -c ENDPOINT)
+num_rds = $(aws rds describe-db-instances | grep -c ENDPOINT)
 echo "# RDS: $num_rds"
 
-num_dynamos=$(aws dynamodb list-tables | wc -l)
+num_dynamos = $(aws dynamodb list-tables | wc -l)
 echo "# DynamoDB: $num_dynamos"
 
-num_elasticache_memcached=$(aws elasticache describe-cache-clusters | grep CACHECLUSTERS | grep -c memcache)
+num_elasticache_memcached = $(aws elasticache describe-cache-clusters | grep CACHECLUSTERS | grep -c memcache)
 echo "# ElastiCache/Memcache: $num_elasticache_memcached"
 
-num_elasticache_redis=$(aws elasticache describe-cache-clusters | grep CACHECLUSTERS | grep -c redis)
+num_elasticache_redis = $(aws elasticache describe-cache-clusters | grep CACHECLUSTERS | grep -c redis)
 echo "# ElastiCache/Redis: $num_elasticache_redis"
 
-num_cloudsearch=$(aws cloudsearch describe-domains | grep -c SEARCHSERVICE)
+num_cloudsearch = $(aws cloudsearch describe-domains | grep -c SEARCHSERVICE)
 echo "# CloudSearch: $num_cloudsearch"
